6583d84b42e7e603da13a7f867d7e1fe31f62293
Tarık Umutlu <tarik_41.41@hotmail.com>
2024-12-30 04:33:40 +0300

    chore: removed ineffective insert query

--- after_tables_created.sql
+++ after_tables_created.sql
@@ -4,5 +4,4 @@ DROP CONSTRAINT IF EXISTS unique_movie_session_constraint;
 ALTER TABLE movie_session
 ADD CONSTRAINT unique_movie_session_constraint UNIQUE NULLS NOT DISTINCT ("date", "time", "roomNumber", "deletedAt");
 
-insert into "user" ("username", "password", "role", "age") values ('admin', 'Admin123.', 'manager', '30');
 insert into "room" ("id", "title") values (1, 'Room 1'), (2, 'Room 2'), (3, 'Room 3');
